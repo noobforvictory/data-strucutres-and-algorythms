6f3aa85f6d1a2312cbbaf5c9fdcbb19d10a9f270
denis <denisvictorjosphine1996@gmail.com>
2022-06-28 12:07:10 +0530

    add problem

--- src/ContainDuplicate.java
+++ src/ContainDuplicate.java
@@ -1,9 +1,23 @@
+import java.util.HashSet;
 
 public class ContainDuplicate {
+	public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer>numbers = new HashSet<Integer>();
+        for(int i=0; i<nums.length; i++){
+
+        if(numbers.contains(nums[i])){
+            return true;
+        }
+            else{
+                numbers.add(nums[i]);
+            }
+        }
+        return false;
+    }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] nums = {1,2,3,4,1};
+       containsDuplicate(nums);
 	}
 
 }
