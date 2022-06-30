03ac2224ba32ddcbcce0362fc99571c0e08b2d90
denis <denisvictorjosphine1996@gmail.com>
2022-06-30 11:04:18 +0530

    impiment sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,9 +1,9 @@
 
 public class BubbleSort {
-
+// in bubble sort we take the biggest element from the start and take it to the end by swapping
 	public static void main(String[] args) {
 		int[] arr= {7,8,1,3,2};
-		for(int i=0; i<arr.length;i++) {
+		for(int i=0; i<arr.length-1;i++) {
 			for(int j=0; j<arr.length-i-1; j++) {
 				if (arr[j]>arr[j+1]) {
 					int temp = arr[j];
