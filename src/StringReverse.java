5254c5473a008399ddfe7d3ff53dfda6aca6e907
denis <denisvictorjosphine1996@gmail.com>
2022-07-09 10:25:44 +0530

    make String reverse programe

---
+++ src/StringReverse.java
@@ -0,0 +1,20 @@
+
+public class StringReverse {
+	public static void reverseString(String str, int index) {
+		if(index==0) {
+			System.out.print(str.charAt(0));
+			return;
+			}
+		System.out.print(str.charAt(index));
+		reverseString(str,index-1);
+		
+	}
+
+	public static void main(String[] args) {
+		String str ="abcd";
+		int index= str.length()-1;
+		reverseString(str,index);
+
+	}
+
+}
